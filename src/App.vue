80c994c5bc6530792b68f68a791c78cae964b0fb
albertopiffer <albertopiffer96@gmail.com>
2023-03-05 18:49:09 +0100

    START Yu-Gi-Oh Api

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import yghHeader from './components/yghHeader.vue'
+
+export default {
+  components: {
+    yghHeader,
+  }
+}
+</script>
+
+<template>
+  <yghHeader />
+</template>
+
+<style lang="scss">
+@use './style/main.scss';
+</style>
---
+++ src/components/yghHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        <h1>Yu-Gi-Oh Api</h1>
+    </header>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+header {
+    height: 105px;
+    background-color: tomato;
+    display: flex;
+    align-items: center;
+
+    padding-left: 140px;
+}
+
+header h1 {
+    font-family: Arial, Helvetica, sans-serif;
+}
+</style>
